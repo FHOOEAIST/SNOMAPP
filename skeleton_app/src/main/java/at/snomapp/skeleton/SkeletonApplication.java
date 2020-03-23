b35cac5b4b4548a934052f25f26f6b68bbdac99d
Elisabeth Mayrhuber <emayrhuber@gmx.at>
2020-03-23 15:51:53 +0100

    integrated Swagger Client into skeleton application

--- skeleton_app/src/main/java/at/snomapp/skeleton/SkeletonApplication.java
+++ skeleton_app/src/main/java/at/snomapp/skeleton/SkeletonApplication.java
@@ -2,8 +2,13 @@ package at.snomapp.skeleton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
+@ComponentScan(basePackages = {"at.snomapp.skeleton"})
 public class SkeletonApplication {
 
     public static void main(String[] args) {
