13c8c922fc10be84cc30a9923ac1addf86ef5b95
Clara <claraj.kainz@gmail.com>
2020-03-23 17:03:59 +0100

    Importer v1.1 zeilenweise einlesen und schön auf konsole printen

--- skeleton_app/src/main/java/at/snomapp/skeleton/importer/Importer.java
+++ skeleton_app/src/main/java/at/snomapp/skeleton/importer/Importer.java
@@ -3,6 +3,7 @@ package at.snomapp.skeleton.importer;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class Importer {
@@ -10,22 +11,40 @@ public class Importer {
     public static void importer_v1(String fileName)throws Exception{
         BufferedReader bReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
         String s;
-        String [] splittedLine = new String[7];
+        String NameSparte;
+        String code;
+        String code1;
+        String code2;
+        String code3;
+        String code4;
+        String code5;
+        String Beschreibung;
+        int i = 0;
+        //ArrayList<String> line = new ArrayList<String>();
+        //String [] splittedLine = new String[7];
         while((s = bReader.readLine())!= null){
-            splittedLine = s.split(";");
-            for (int i = 0; i < splittedLine.length; i++) {
-                System.out.print(splittedLine[i] +", ");
-            }
-            System.out.println();
-
+                NameSparte = s.split(";")[0];
+                code = s.split(";")[1];
+                code1 = s.split(";")[2];
+                code2 = s.split(";")[3];
+                code3 = s.split(";")[4];
+                code4 = s.split(";")[5];
+                code5 = s.split(";")[6];
+                Beschreibung = s.split(";")[7];
+                if(!NameSparte.isEmpty()){
+                System.out.println(NameSparte);
+                } else {
+                System.out.println(" "+code +
+                        ", " + code1 + " " +code2 +
+                        " " + code3 + " " + code4 +
+                        " " + code5 + ", "+Beschreibung);
+                }
         }
         bReader.close();
 
     }
 
     public static void main(String[] args) throws Exception {
-
-        System.out.println("Test version 1:");
         importer_v1("src/main/resources/APPC_machinereadable.csv");
     }
 }
