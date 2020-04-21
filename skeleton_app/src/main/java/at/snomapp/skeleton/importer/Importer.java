454555845e16b14cac9657166fbb0104a7926b90
Clara <claraj.kainz@gmail.com>
2020-04-21 10:58:55 +0200

    Swagger Client

--- skeleton_app/src/main/java/at/snomapp/skeleton/importer/Importer.java
+++
@@ -1,7 +0,0 @@
-package at.snomapp.skeleton.importer;
-
-import at.snomapp.skeleton.APPC.APPCTree;
-//interface for importer
-public interface Importer {
-    APPCTree importTree(String fileName) throws Exception;
-}
