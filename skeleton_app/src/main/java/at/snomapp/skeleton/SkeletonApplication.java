75674531f25dd7b44e640097daaba4a3b4fd36e1
David Rainer <rainer.david@hotmail.com>
2020-05-18 14:34:08 +0200

    removed swaggerUI dependencies
    caused a conflict with neo4j classloader

--- skeleton_app/src/main/java/at/snomapp/skeleton/SkeletonApplication.java
+++ skeleton_app/src/main/java/at/snomapp/skeleton/SkeletonApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+// ! conflicts with neo4j
+// ! removed until bug is fixed (see trello board)
+//import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 
 @SpringBootApplication
-@EnableSwagger2WebMvc
-@EntityScan(basePackages = {"at.snomapp.skeleton.appc"})
+// ! conflicts with neo4j
+// ! removed until bug is fixed (see trello board)
+//@EnableSwagger2WebMvc
+@EntityScan(basePackages = "at.snomapp.skeleton.appc")
 @EnableNeo4jRepositories(basePackages = "at.snomapp.skeleton.repo")
 public class SkeletonApplication {
 
