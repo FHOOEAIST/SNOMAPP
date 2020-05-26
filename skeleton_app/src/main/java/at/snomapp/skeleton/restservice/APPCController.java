17897ad0bc42c31a2eb35c66f9687491dc4fbb0c
David Rainer <rainer.david@hotmail.com>
2020-05-26 12:55:50 +0200

    Added Endpoints for querying APPC Codes based on id and displayName

--- skeleton_app/src/main/java/at/snomapp/skeleton/restservice/APPCController.java
+++ skeleton_app/src/main/java/at/snomapp/skeleton/restservice/APPCController.java
@@ -9,12 +9,13 @@ import at.snomapp.skeleton.repo.APPCRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.Set;
 
 @RestController
 @RequestMapping("/APPC")
-// controller providing endpoints for importing and retreiving APPCData
+// controller providing endpoints for importing and retrieving APPCData
 public class APPCController {
 
     private final APPCRepo repo;
@@ -25,22 +26,33 @@ public class APPCController {
     }
 
     @DeleteMapping
-    // flushes databank
+    // flushes data bank
     // to be used for debugging only
     // remove from release version
     void clearDB(){
         repo.deleteAll();
     }
 
-    @GetMapping("/find")
-    // finds a node based on the displayName
-    Entry findEntry(@RequestParam String displayName){
-        return repo.findByDisplayName(displayName);
+    @GetMapping("entry/{id}")
+    Entry readByIdentity(@PathVariable Long id){
+        Optional<Entry> byId = repo.findById(id);
+        return byId.orElse(null);
+    }
+
+    @GetMapping("entry")
+    Iterable<Entry> readByDisplayName(@RequestParam(required = false) String displayName){
+        if(displayName == null){
+            // all entries
+            return repo.findAll();
+        }else{
+            // return exact name match
+            return repo.findAllByDisplayNameContainingIgnoreCase(displayName);
+        }
     }
 
     @PostMapping("/import")
     // imports an APPCtree from a given filename into the neo4j database.
-    // clears databank first one ach call
+    // clears data bank first on each call
     void importAPPC(@RequestBody String filename){
         repo.deleteAll();
         Importer importer = new CSVImporter();
@@ -56,7 +68,18 @@ public class APPCController {
         }
     }
 
-    // needs tweeking if multiple languages are supported
+    @GetMapping("roots")
+    // return just the tree roots
+    Iterable<Entry> readRoots(){
+        Entry anatomy = repo.findByDisplayName("Anatomy");
+        Entry laterality = repo.findByDisplayName("Laterality");
+        Entry modality = repo.findByDisplayName("Modality");
+        Entry procedure = repo.findByDisplayName("Procedures");
+
+        return Arrays.asList(anatomy, laterality, modality, procedure);
+    }
+
+    // needs tweaking if multiple languages are supported
     @GetMapping
     // returns whole tree saved in Data bank
     APPCTree getTree(){
