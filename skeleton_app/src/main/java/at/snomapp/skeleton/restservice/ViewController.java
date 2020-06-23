457c3b06582c1710573161876cf2fb8c9e61646f
Kathrin Gutenbrunner <kathrin.gutenbrunner@gmail.com>
2020-06-23 14:27:55 +0200

    with .iml file

--- skeleton_app/src/main/java/at/snomapp/skeleton/restservice/ViewController.java
+++ skeleton_app/src/main/java/at/snomapp/skeleton/restservice/ViewController.java
@@ -4,6 +4,7 @@ package at.snomapp.skeleton.restservice;
 import at.snomapp.skeleton.domain.appc.APPCTree;
 import at.snomapp.skeleton.domain.appc.Entry;
 import at.snomapp.skeleton.repo.APPCRepo;
+import com.sun.source.tree.Tree;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,10 +33,11 @@ public class ViewController {
         APPCController appcController = new APPCController(repo);
 
         //store in model the json string of nodes for each axis
-        model.addAttribute("anatomy", appcController.getTree().getAnatomyJsonString());
-        model.addAttribute("laterality", appcController.getTree().getLateralityJsonString());
-        model.addAttribute("modality", appcController.getTree().getModalityJsonString());
-        model.addAttribute("procedure", appcController.getTree().getProcedureJsonString());
+        APPCTree tree = appcController.getTree();
+        model.addAttribute("anatomy", tree.getAnatomyJsonString());
+        model.addAttribute("laterality", tree.getLateralityJsonString());
+        model.addAttribute("modality", tree.getModalityJsonString());
+        model.addAttribute("procedure", tree.getProcedureJsonString());
         return "index";
     }
 
