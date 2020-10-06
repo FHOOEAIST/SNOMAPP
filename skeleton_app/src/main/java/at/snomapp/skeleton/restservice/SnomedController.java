874e8faeaaad7bbaf5ecb4848270fa2d264633a4
Elisabeth <emayrhuber@gmx.at>
2020-10-06 11:48:29 +0200

    add synonyms

--- skeleton_app/src/main/java/at/snomapp/skeleton/restservice/SnomedController.java
+++ skeleton_app/src/main/java/at/snomapp/skeleton/restservice/SnomedController.java
@@ -3,13 +3,18 @@ package at.snomapp.skeleton.restservice;
 import io.swagger.client.ApiException;
 import io.swagger.client.api.DescriptionsApi;
 import io.swagger.client.model.BrowserDescriptionSearchResult;
+import io.swagger.client.model.CollectionDescription;
+import io.swagger.client.model.ItemsPageDescription;
 import io.swagger.client.model.PageBrowserDescriptionSearchResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("snomed")
@@ -55,5 +60,27 @@ public class SnomedController {
         // might want to replace with wrapped PageBrowserDescriptionSearchResult depending on if we need the
         // meta information (e.g. size of returned array) aor not
         return response != null ? response.getItems() : null;
+    }
+
+    Map<BrowserDescriptionSearchResult, CollectionDescription> findSynonyms(List<BrowserDescriptionSearchResult> concepts){
+        Map<BrowserDescriptionSearchResult, CollectionDescription> descriptionMap = new HashMap<>();
+        List<CollectionDescription> response = new ArrayList<>();
+        for (BrowserDescriptionSearchResult concept : concepts) {
+            String branch = "MAIN";
+            String conceptId = concept.getConcept().getConceptId();
+            Integer offset=0;
+            Integer limit = null;
+
+            ItemsPageDescription response_item = null;
+            try {
+                response_item = api.findDescriptionsUsingGET(branch, conceptId, offset, limit);
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+
+            descriptionMap.put( concept, response_item != null ? response_item.getItems() : null);
+        }
+
+        return descriptionMap;
     }
 }
--- skeleton_app/src/main/java/at/snomapp/skeleton/restservice/ViewController.java
+++ skeleton_app/src/main/java/at/snomapp/skeleton/restservice/ViewController.java
@@ -10,6 +10,7 @@ import at.snomapp.skeleton.repo.APPCRepo;
 import at.snomapp.skeleton.repo.ConceptMapRepo;
 import at.snomapp.skeleton.repo.MappingRepo;
 import io.swagger.client.model.BrowserDescriptionSearchResult;
+import io.swagger.client.model.CollectionDescription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -65,8 +67,10 @@ public class ViewController {
         if(byId.isPresent()){
             Entry entry = byId.get();
             List<BrowserDescriptionSearchResult> resultList = snomedController.findByDisplayName(entry.getDisplayName());
+            Map<BrowserDescriptionSearchResult, CollectionDescription> resultMap = snomedController.findSynonyms(resultList);
             List<EquivalenceImpl> mappings = new ArrayList<>();
             model.addAttribute("results",resultList);
+            model.addAttribute("resMap", resultMap);
             model.addAttribute("appc", entry);
             //ToDo
             model.addAttribute("mappings", mappings);
