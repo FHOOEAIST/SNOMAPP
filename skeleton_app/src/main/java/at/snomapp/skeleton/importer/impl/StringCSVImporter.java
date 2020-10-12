d132ffdb97abb23a1ed07a0ffa8e0decd83d37b4
David Rainer <rainer.david@hotmail.com>
2020-10-12 13:40:48 +0200

    Initial commit

---
+++ skeleton_app/src/main/java/at/snomapp/skeleton/importer/impl/StringCSVImporter.java
@@ -0,0 +1,14 @@
+package at.snomapp.skeleton.importer.impl;
+
+import at.snomapp.skeleton.domain.appc.APPCTree;
+import at.snomapp.skeleton.importer.Importer;
+
+
+// importer receiving plain text csv and not a filepath
+public class StringCSVImporter implements Importer {
+    // TODO: 12.10.2020 implement 
+    @Override
+    public APPCTree importTree(String from) throws Exception {
+        return null;
+    }
+}
